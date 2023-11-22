e0a7e73110f47b684d03c590fbbcedb1fd8ce6ec
Aniket Umredkar <aniketumredkar@plobalapps.com>
2023-11-22 13:33:23 +0530

    fetch user data

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,17 +1,49 @@
-import React from "react";
+import React, { useCallback, useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
+import { getUserData } from "../apiHelpers/auth";
 
 const Navbar = () => {
+    const [firstName, setFirstName] = useState("");
+    const [lastName, setLastName] = useState("");
+
     const navigate = useNavigate();
     const logOutHandler = () => {
         window.localStorage.removeItem("token");
         navigate("/login");
     };
+
+    const fetchUserData = useCallback(() => {
+        getUserData()
+            .then((res) => {
+                if (res.status === 200) {
+                    res.json()
+                        .then((res) => {
+                            setFirstName(res.first_name);
+                            setLastName(res.last_name);
+                        })
+                        .catch((err) => {
+                            console.log(err);
+                        });
+                } else {
+                    navigate("/login");
+                }
+            })
+            .catch((err) => {
+                console.log(err);
+                window.localStorage.removeItem("token");
+                navigate("/login");
+            });
+    }, [navigate]);
+
+    useEffect(() => {
+        fetchUserData();
+    }, [fetchUserData]);
+
     return (
         <nav className="navbar navbar-expand-lg navbar-light bg-light">
             <div className="container-fluid">
                 <a className="navbar-brand" href="/">
-                    Home
+                    Hi {firstName} {lastName}!
                 </a>
                 <div className="collapse navbar-collapse d-flex justify-content-end" id="navbarNav">
                     <ul className="navbar-nav">
